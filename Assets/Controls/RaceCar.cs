4fbe3d808063ee2e69fbb880735b8ff91f6b67bc
cjayneb <70963776+cjayneb@users.noreply.github.com>
2025-06-18 21:30:26 -0400

    End to end works
    - Controls sent from oculus touch controllers to robot
    - Video received from robot
    - First driving test
    - video stream Display resized

---
+++ Assets/Controls/RaceCar.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+
+public class RaceCar
+{
+    private const float Max = 1;
+    public float throttle = 0;
+    public float steering = 0;
+
+    public float Throttle
+    {
+        get { return throttle; }
+        set
+        {
+            if (value > 0 && throttle < 0 || value < 0 && throttle > 0)
+            {
+                throttle = 0;
+                return;
+            }
+
+            throttle = value > Max ? Max : (value < -Max ? -Max : value);
+        }
+    }
+
+    public float Steering
+    {
+        get { return steering; }
+        set
+        {
+            if (value > 0 && steering < 0 || value < 0 && steering > 0)
+            {
+                steering = 0;
+                return;
+            }
+
+            steering = value > Max ? Max : (value < -Max ? -Max : value);
+        }
+    }
+}
---
+++ realSteelRunner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd oculus-build && adb uninstall com.realsteelproject.oculusapp && adb install oculus-app-0.1.0.apk && adb shell am start -n com.realsteelproject.oculusapp/com.unity3d.player.UnityPlayerGameActivity
